8c72649546fe00aff373b9adedac8c76b7d31b0a
PlayerTv9 <tommaso.ibba09@gmail.com>
2022-04-08 09:08:49 +0200

    ver 1.0

---
+++ Gimp e altro/Untitled-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Mi piace il c++, e il mio linguaggio di programmazione preferito!!!"<<endl;
+    return 0;
+}
+
+int main()
+{
+    cout<<"Mi piace il c++, e il mio linguaggio di programmazione preferito!!!"<<endl;
+    return 0;
+}
+
+int main()
+{
+    cout<<"Mi piace il c++, e il mio linguaggio di programmazione preferito!!!"<<endl;
+    return 0;
+}
+
+int main()
+{
+    cout<<"Mi piace il c++, e il mio linguaggio di programmazione preferito!!!"<<endl;
+    return 0;
+}
+int main()
+{
+    cout<<"Mi piace il c++, e il mio linguaggio di programmazione preferito!!!"<<endl;
+    return 0;
+}
